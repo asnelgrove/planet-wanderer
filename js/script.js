33eba16c8d0a296b82dec97ac0ecd2fcfa672da2
Andrew Snelgrove <asnel@my.yorku.ca>
2023-04-20 10:47:14 -0230

    fix img url

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('menu-btn');
 const mobmenu = document.getElementById('menu');
+const year = document.querySelector('.year');
+year.innerHTML = new Date().getFullYear();
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
